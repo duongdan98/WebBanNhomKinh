10399670b1148aed9c9ece299b2d9ea63ec9f904
Duong Dan <duongvandan2806@gmail.com>
2021-10-23 06:07:56 +0700

    add admin quanlysanpham

---
+++ WebBanNhomKinh/Models/QuanLySanPham.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+namespace WebBanNhomKinh.Models
+{
+    public class QuanLySanPham
+    {
+        public IPagedList<SanPham> ListSanPham { get; set; }
+        public SanPham SanPham { get; set; }
+        public ChiTietSanPham ChiTietSanPham { get; set; }
+    }
+}
