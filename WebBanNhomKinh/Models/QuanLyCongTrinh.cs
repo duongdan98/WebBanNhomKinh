a6b8661fc8650e178ce558d1d4e63a530242d145
William <williamdunbar2000@gmail.com>
2021-10-24 09:31:19 +0700

    edit function in admin interface

---
+++ WebBanNhomKinh/Models/QuanLyCongTrinh.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+
+namespace WebBanNhomKinh.Models
+{
+    public class QuanLyCongTrinh
+    {
+        public IPagedList<CongTrinhNoiBat> DanhSachCongTrinh { get; set; }
+        public CongTrinhNoiBat CongTrinh { get; set; }
+    }
+}
---
+++ WebBanNhomKinh/Models/QuanLyTinTuc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+
+namespace WebBanNhomKinh.Models
+{
+    public class QuanLyTinTuc
+    {
+        public  IPagedList<TinTuc> DanhSachTinTuc { get; set; }
+        public TinTuc TinTuc { get; set; }
+       
+    }
+}
